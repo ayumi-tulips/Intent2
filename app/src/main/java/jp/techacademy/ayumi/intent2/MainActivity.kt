1fdf799b453c9a5d7cc99d646c409f0ad57492b5
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-06-23 20:01:14 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ayumi/intent2/MainActivity.kt
@@ -0,0 +1,22 @@
+package jp.techacademy.ayumi.intent2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT,editText .text.toString())
+            startActivity(intent)
+        }
+    }
+}
